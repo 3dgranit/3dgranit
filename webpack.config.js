6e8d1377067ee9dd8a6a26aadfe593f2c12b10d0
nesta_dima <nesta_dima@mail.ru>
2023-12-21 15:31:18 +0400

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -3,63 +3,66 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index-bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: { 
-            loader: "babel-loader",
-            options: {
-                presets: [
-                    "@babel/preset-env",
-                    "@babel/preset-react"
-                ].map(require.resolve)
-            }
-        }
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index-bundle.js"
     },
-    {
-        test: /\.css$/,
-        use: [
-            {loader: "style-loader"},
-            {loader: "css-loader"}
-        ]
-    },
-    {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
+    module: {
+        rules: [
             {
-                loader: 'url-loader',
-                options: {
-                    limit: 8192
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react"
+                        ].map(require.resolve)
+                    }
                 }
-            }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {loader: "style-loader"},
+                    {loader: "css-loader"}
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
+            },
         ]
     },
-    {
-        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-            loader: 'file-loader',
-            options: {
-                name: '[name].[ext]',
-                outputPath: 'fonts/'
-            }
-        }]
+    optimization: {
+        minimize: false
     },
-      {
-        test: /\.svg$/,
-        loader: 'svg-inline-loader'
-      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
-  ]
 };
